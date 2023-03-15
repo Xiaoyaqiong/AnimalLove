8fa5666e201e7112437570cc9d95f9ec5fe290da
Xiaoyaqiong <1063140183@qq.com>
2023-03-15 10:11:07 +0800

    test

---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="content">
+		<view class="u-demo-block">
+		        <text class="u-demo-block__title">卡片式</text>
+		        <u-swiper
+		                :list="list3"
+		                previousMargin="30"
+		                nextMargin="30"
+		                circular
+		                :autoplay="false"
+		                radius="5"
+		                bgColor="#ffffff"
+		        ></u-swiper>
+		    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list3: [
+				                    'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+				                    'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+				                    'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+				                ],
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+
+</style>
