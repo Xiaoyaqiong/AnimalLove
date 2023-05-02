d1a8c7b49ba2c77f13580abdba505ecf7c0c4bf1
Your Name <826697618@qq.com>
2023-05-02 17:11:57 +0800

    NavBar组件编写，同城夜编写

--- components/MyNavbar.vue
+++ components/MyNavbar.vue
@@ -1,9 +1,10 @@
 <template>
 		<view class="navbar">
+			
 			<u-navbar :fixed="true" height="7vh" :placeholder="true" bgColor="#FFB600">
 				
 				<view class="u-nav-slot" slot="left">
-					<u-search action-text="" class="search" placeholder="布偶要多少钱?"  v-model="keyword"></u-search>
+					<u-search bgColor="#fff" action-text="" class="search" placeholder="布偶要多少钱?"  v-model="keyword"></u-search>
 				</view>
 				<view class="u-nav-slot" slot="right">
 					<u-icon name="chat" space="0" size="1.5rem"></u-icon>
--- pages/sameCity/sameCity.vue
+++ pages/sameCity/sameCity.vue
@@ -1,17 +1,63 @@
 <template>
 	<view class="">
-		<MyBar> </MyBar>
+
+		<MyBar></MyBar>
+		<!-- <view class="status_bar" style="height: var(--status-bar-height); width: 100%;"></view> -->
+		<!-- 三个按钮组 -->
+		<view class="btnGroup">
+			<view class="btn" v-for="(item,index) in btnGroup">
+				<u-icon top="2vh" size="2rem" labelPos="bottom" space="5.6vh" labelSize=".8rem" :label="item"
+					name="red-packet"></u-icon>
+			</view>
+
+		</view>
+
+		<!-- 邀请进群 -->
+		<view class="QQQun">
+			<u-avatar-group :urls="urls" size="2rem" gap="0.4"></u-avatar-group>
+
+			<p>邀请您加入附近的宠圈交流群~</p>
+			<view class="joinInbtn">
+				<u-button color="#FF6900" shape="circle" text="立即加入"></u-button>
+			</view>
+		</view>
+
+
+		<!-- 中间轮播图 -->
+		<view class="Middleswiper">
+			<u-swiper :list="swiperList" indicator indicatorMode="dot" circular></u-swiper>
+		</view>
+
+
 	</view>
 </template>
 
 <script>
 	import MyBar from '@/components/MyNavbar.vue'
 	export default {
-		components:{
+		data() {
+			return {
+				btnGroup: ['商家', '救助/寻宠', '领养'],
+				urls: [
+					'https://cdn.uviewui.com/uview/album/1.jpg',
+					'https://cdn.uviewui.com/uview/album/2.jpg',
+					'https://cdn.uviewui.com/uview/album/3.jpg',
+
+
+				],
+				swiperList: [
+					'https://cdn.uviewui.com/uview/album/1.jpg',
+					'https://cdn.uviewui.com/uview/album/2.jpg',
+					'https://cdn.uviewui.com/uview/album/3.jpg',
+				]
+			}
+		},
+		components: {
 			MyBar
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+	@import 'sameCity.scss';
 </style>
